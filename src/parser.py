27f4b14699ed348060ab5dc0fa5408c928ff5b67
Adam <ciernya@gmail.com>
2020-10-18 15:23:19 +0200

    writing to json file

--- src/parser.py
+++ src/parser.py
@@ -3,7 +3,13 @@ import re
 DATA_DIR_NAME = "data/"
 DATA_FILE_NAME = "smaller_data.txt"
 NEEDED_ATRIBUTES = "(type\.object\.name|\.display_name|type\.object\.type)" #\#label|\#type
+OUTPUT_FILE_NAME = "parsed_objects.json"
 
+json_file = open("{}{}".format(DATA_DIR_NAME, OUTPUT_FILE_NAME),"w")
+json_file.write("{\n")
+
+def print_object_to_file(object, title):
+    print("\"{}\": {},".format(title,object[title]).replace('\'','\"'),file=json_file)
 
 with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),"r") as file:
     parsed_object = {}
@@ -11,7 +17,6 @@ with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),"r") as file:
     for line in file:
         if(re.search(NEEDED_ATRIBUTES, line)):
             triplet = line.split("\t")
-            #print("first line: {}\nsecond line: {}\nthird line: {}".format(triplet[0], triplet[1], triplet[2]))
             temp = triplet[0].split('.')
             line_title = temp[len(temp)-1].replace('>','')
 
@@ -20,18 +25,17 @@ with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),"r") as file:
                 parsed_object[line_title]={}
             elif line_title != parsed_title:
                 print(parsed_object)
+                print_object_to_file(parsed_object, parsed_title)
                 parsed_object={}
                 parsed_title = line_title
                 parsed_object[line_title]={}
-                #also index object
-
+                #also index objects
             attribute=""
 #            if("#" in triplet[1]):
 #                attribute = triplet[1].split('#')[1].replace('>','')
 #            else:
             temp = triplet[1].split('.')
             attribute = temp[len(temp)-1].replace('>','')
-
             object=""
 #            if('#' in triplet[2]):
 #                object=triplet[2].split('#')[1].replace('>','')
@@ -43,5 +47,7 @@ with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),"r") as file:
             parsed_object[parsed_title][attribute] = object
     print(parsed_object)
     parsed_object={}
-            
+
+json_file.write("\"end\":{ }\n}")
+json_file.close()
 #Need to refine the parsing of attributes - if name/label/we, put it into alt_title arr probably
