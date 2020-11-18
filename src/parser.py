ffe47bcf53979627511daa8d119c19349d90ac52
ciernya <ciernya@gmail.com>
2020-11-18 19:11:56 +0100

    edited code for the fifth consultation

--- src/parser.py
+++ src/parser.py
@@ -1,39 +1,69 @@
 import re
 import json
-from elasticsearch import Elasticsearch
+import zlib
+from elasticsearch import Elasticsearch, helpers
 
 DATA_DIR_NAME = "data/"
-DATA_FILE_NAME = "big_data.txt"
-NEEDED_ATRIBUTES = "(type\.object\.name|\.display_name|type\.object\.type)" #\#label|\#type
+DATA_FILE_NAME = "freebase-rdf-latest.gz"
+NEEDED_ATRIBUTES = "(type\.object\.name|\.display_name|type\.object\.type)"
+NOT_IMPORTANT_ATTRIBUTES = "(www\.w3\.org|dated_percentage|dated_integer|pronunciation|pagination)"
+#Further filter out: 
 ENGLISH_NAME = ""
-OUTPUT_FILE_NAME = "parsed_objects.json"
+OUTPUT_FILE_NAME = "parsed_objects"
 
-json_file = open("{}{}".format(DATA_DIR_NAME, OUTPUT_FILE_NAME),"w", encoding="utf-8", errors="replace")
+def stream_unzipped_bytes(filename):
+    with open(filename, 'rb') as f:
+        wbits = zlib.MAX_WBITS | 16
+        to_decompress = zlib.decompressobj(wbits)
+        fbytes = f.read(16384)
+        while fbytes:
+            yield to_decompress.decompress(to_decompress.unconsumed_tail + fbytes)
+            fbytes = f.read(16384)
+
+def stream_text(gen):
+    try:
+        buffer = next(gen)
+        while buffer:
+            lines = buffer.splitlines(keepends=True)
+            for line in lines[:-1]:
+                yield line.decode() #encoding='UTF-8'
+            buffer = lines[-1]
+            buffer += next(gen)
+            if(len(buffer) < 500):
+                print(f"{len(buffer)} - content of buffer: {buffer}")
+    except StopIteration:
+        print("I got here")
+        if buffer:
+            yield buffer.decode() #encoding='UTF-8'
+
+
+json_file = open("{}{}".format(DATA_DIR_NAME, OUTPUT_FILE_NAME),"w", encoding="utf-8") #, errors="replace"
 json_file.write("[\n")
 
 indexer = Elasticsearch()
-index_id = 0
 
-indexer.indices.delete(index="_all")
+indexer.indices.delete(index="vinf_index")
 
 def print_object_to_file(object):
     print("{},".format(json.dumps(object, sort_keys=True)),file=json_file)
 
 def parse_object_triplet(part):
-#    if('#' in part):
-#        return part.split('#')[1].replace('>','')
-    if('"' in part):
-        return part.split('"')[1]
-    else:
+    if('@' in part):
+        return part.split('@')[0].strip('"')
+    if('.'):
         temp = part.split('.')
         return temp[len(temp)-1].replace('>','')
+    else:
+        return part
 
-
-
-with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),encoding="utf-8") as file:
+def handle_data():
+    index_id = 0
+    b_generator = (x for x in stream_unzipped_bytes("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME)))
     parsed_object = {}
     parsed_title=""
-    for line in file:
+    for line in stream_text(b_generator):
+        if (re.search(NOT_IMPORTANT_ATTRIBUTES, line)):
+            continue
         if(re.search(NEEDED_ATRIBUTES, line)):
             triplet = line.split("\t")
             temp = triplet[0].split('.')
@@ -45,10 +75,15 @@ with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),encoding="utf-8") as file:
                 parsed_object["title"] = parsed_title
             elif line_title != parsed_title:
                 print_object_to_file(parsed_object)
-                indexer.index(index = "vinf_index", id=index_id, body = parsed_object) 
+                yield {
+                    "_index": "vinf_index",
+                    "_id": index_id,
+                    "_source": parsed_object
+                }
                 index_id += 1
 
                 parsed_title = line_title
+                del parsed_object
                 parsed_object={}
                 parsed_object["title"] = parsed_title
             attribute=""
@@ -62,13 +97,12 @@ with open("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME),encoding="utf-8") as file:
             object = parse_object_triplet(triplet[2])
             if(re.search("(name|display_name)",attribute)):                
                 attribute = "name" if attribute == "display_name" else attribute
-                if(attribute not in parsed_object):
-                    parsed_object[attribute] = []
-                if(object not in parsed_object[attribute]):
-                    parsed_object[attribute].append(object)
-            else:
-                parsed_object[attribute] = object
-    parsed_object={}
+            if(attribute not in parsed_object):
+                parsed_object[attribute] = []
+            if(object not in parsed_object[attribute]):
+                parsed_object[attribute].append(object)
+    del parsed_object
 
+helpers.bulk(indexer, handle_data())
 json_file.write("\n]")
 json_file.close()
