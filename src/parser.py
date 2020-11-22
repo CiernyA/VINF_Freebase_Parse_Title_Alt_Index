f378dcbf28f969b711b5759c066958ebdefc0983
Adam <ciernya@gmail.com>
2020-11-22 11:20:54 +0100

    Edited to run on the whole unpacked thing

--- src/parser.py
+++ src/parser.py
@@ -4,7 +4,8 @@ import zlib
 from elasticsearch import Elasticsearch, helpers
 
 DATA_DIR_NAME = "data/"
-DATA_FILE_NAME = "freebase-rdf-latest.gz"
+DATA_FILE_NAME = "freebase-rdf-latest"
+#DATA_FILE_NAME = "freebase-rdf-latest.gz"
 NEEDED_ATRIBUTES = "(type\.object\.name|\.display_name|type\.object\.type)"
 NOT_IMPORTANT_ATTRIBUTES = "(www\.w3\.org|dated_percentage|dated_integer|pronunciation|pagination)"
 #Further filter out: 
@@ -42,7 +43,7 @@ json_file.write("[\n")
 
 indexer = Elasticsearch()
 
-indexer.indices.delete(index="vinf_index")
+#indexer.indices.delete(index="vinf_index")
 
 def print_object_to_file(object):
     print("{},".format(json.dumps(object, sort_keys=True)),file=json_file)
@@ -61,46 +62,48 @@ def handle_data():
     b_generator = (x for x in stream_unzipped_bytes("{}{}".format(DATA_DIR_NAME,DATA_FILE_NAME)))
     parsed_object = {}
     parsed_title=""
-    for line in stream_text(b_generator):
-        if (re.search(NOT_IMPORTANT_ATTRIBUTES, line)):
-            continue
-        if(re.search(NEEDED_ATRIBUTES, line)):
-            triplet = line.split("\t")
-            temp = triplet[0].split('.')
-            line_title = temp[len(temp)-1].replace('>','')
+    #for line in stream_text(b_generator):
+    with open("{}{}".format(DATA_DIR_NAME, DATA_FILE_NAME),'r', encoding='UTF-8') as file:
+        for line in file:
+            if (re.search(NOT_IMPORTANT_ATTRIBUTES, line)):
+                continue
+            if(re.search(NEEDED_ATRIBUTES, line)):
+                triplet = line.split("\t")
+                temp = triplet[0].split('.')
+                line_title = temp[len(temp)-1].replace('>','')
 
-            if parsed_title == "":
-                parsed_title = line_title
-                parsed_object={}
-                parsed_object["title"] = parsed_title
-            elif line_title != parsed_title:
-                print_object_to_file(parsed_object)
-                yield {
-                    "_index": "vinf_index",
-                    "_id": index_id,
-                    "_source": parsed_object
-                }
-                index_id += 1
+                if parsed_title == "":
+                    parsed_title = line_title
+                    parsed_object={}
+                    parsed_object["title"] = parsed_title
+                elif line_title != parsed_title:
+                    print_object_to_file(parsed_object)
+                    yield {
+                        "_index": "vinf_index",
+                        "_id": index_id,
+                        "_source": parsed_object
+                    }
+                    index_id += 1
 
-                parsed_title = line_title
-                del parsed_object
-                parsed_object={}
-                parsed_object["title"] = parsed_title
-            attribute=""
+                    parsed_title = line_title
+                    del parsed_object
+                    parsed_object={}
+                    parsed_object["title"] = parsed_title
+                attribute=""
 
-            temp = triplet[1].split('.')
-            attribute = temp[len(temp)-1].replace('>','')
-            if("@" in triplet[2]):
-                if not re.search("@en",triplet[2]):
-                    continue
+                temp = triplet[1].split('.')
+                attribute = temp[len(temp)-1].replace('>','')
+                if("@" in triplet[2]):
+                    if not re.search("@en",triplet[2]):
+                        continue
 
-            object = parse_object_triplet(triplet[2])
-            if(re.search("(name|display_name)",attribute)):                
-                attribute = "name" if attribute == "display_name" else attribute
-            if(attribute not in parsed_object):
-                parsed_object[attribute] = []
-            if(object not in parsed_object[attribute]):
-                parsed_object[attribute].append(object)
+                object = parse_object_triplet(triplet[2])
+                if(re.search("(name|display_name)",attribute)):                
+                    attribute = "name" if attribute == "display_name" else attribute
+                if(attribute not in parsed_object):
+                    parsed_object[attribute] = []
+                if(object not in parsed_object[attribute]):
+                    parsed_object[attribute].append(object)
     del parsed_object
 
 helpers.bulk(indexer, handle_data())
